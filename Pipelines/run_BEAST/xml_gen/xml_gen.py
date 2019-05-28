76b66561fe68e500be12e99ea0b91f49f9960ec1
uym2 <ceciliamaitouyen@gmail.com>
2019-05-27 18:07:51 -0700

    run BEAST with no dating

--- Pipelines/run_BEAST/xml_gen/xml_gen.py
+++ Pipelines/run_BEAST/xml_gen/xml_gen.py
@@ -5,7 +5,7 @@ from sequence_lib import read_fasta
 import re
 
 template_file = argv[1]
-sampling_time = argv[2]
+sampling_time = argv[2] 
 sequence_file = argv[3]
 tree_file = argv[4]
 log_file = argv[5]
@@ -26,13 +26,14 @@ def write_alignment(sequence_file,fout):
                 fout.write("\t\t\t"+seq + "\n")
                 fout.write("\t\t</sequence>\n")
 
-def write_sampling_time(infile,fout):
+def write_sampling_time(infile,fout,write_date=True):
     with open(infile,'r') as fin:
         fin.readline()
         for line in fin:
             taxon, time = line.strip().split()
             fout.write("\t\t<taxon id=\"" + taxon + "\">\n")
-            fout.write("\t\t\t<date value=\"" + time + "\" direction=\"forwards\" units=\"years\"/>\n")
+            if time and write_date:
+                fout.write("\t\t\t<date value=\"" + time + "\" direction=\"forwards\" units=\"years\"/>\n")
             fout.write("\t\t</taxon>\n")
 
 with open(template_file,'r') as fin:
@@ -43,8 +44,9 @@ with open(template_file,'r') as fin:
             if line.strip()[:8] == "<taxa id":
                 taxa_start = line
                 break
-                
-        write_sampling_time(sampling_time,fout)
+        
+        has_date = True if (sampling_time[-1] == '1') else False        
+        write_sampling_time(sampling_time[:-1],fout,write_date=has_date)
         
         while 1:
             line = fin.readline()
--- Results/results_all.R
+++ Results/results_all.R
@@ -4,19 +4,43 @@ require(ggplot2)
 
 d = read.table("results_all.txt")
 colnames(d) = c("set","rep","method","treeTool","clockModel","benchmark","measure","value")
+d$method = factor(d$method,levels=c("BEAST_strict","BEAST_lnorm","lsd","LF","logDate","wlD"))
+d$clockModel = factor(d$clockModel,levels=c("lognorm","gamma"))
 
-ggplot(d[d$treeTool != "RAxML",],aes(x=clockModel,y=value,group=interaction(method,clockModel),fill=method)) +
+ggplot(d[d$treeTool != "RAxML" & d$measure=="rmse",],aes(x=clockModel,y=value,group=interaction(method,clockModel),fill=method)) +
   geom_boxplot(outlier.size = 0.5,outlier.alpha = 0.5) + theme_bw() +
   facet_wrap(~benchmark+set,scale="free",nrow = 2) + ylab("rmse") +
   theme(legend.position = "bottom", legend.title = element_blank(),axis.title.x = element_blank()) +
-  scale_fill_brewer(palette = "Accent")
-
+  scale_fill_brewer(palette = "Dark2")
 ggsave("Four_settings.pdf")
 
-ggplot(d[d$treeTool != "RAxML",],aes(x=method,y=value,group=method,fill=method)) +
-  geom_bar(stat="summary") + geom_errorbar(stat="summary",width=0.4) +
+ggplot(d[d$treeTool != "RAxML" & d$measure=="rmse" & d$benchmark=="brTime",],aes(x=clockModel,y=value,group=interaction(method,clockModel),fill=method)) +
+  #geom_bar(stat="summary",position="dodge",alpha=0.85) + 
+  #geom_errorbar(stat="summary",position="dodge",aes(color=method))+ 
+  stat_summary(size=0.1,aes(color=method),position=position_dodge(width = 0.5)) +
+  theme_bw() +
+  facet_wrap(~set,scale="free") + ylab("rmse") +
+  theme(legend.position = "bottom", legend.title = element_blank(),axis.title.x = element_blank()) +
+  scale_fill_brewer(palette = "Dark2")+
+  scale_color_brewer(palette = "Dark2")
+ggsave("Four_settings_brTime.pdf")
+
+ggplot(d[d$treeTool != "RAxML" & d$measure=="rmse" & d$benchmark=="nodeAge",],aes(x=clockModel,y=value,group=interaction(method,clockModel),fill=method)) +
+  #geom_bar(stat="summary",position="dodge",alpha=0.85) + 
+  #geom_errorbar(stat="summary",position="dodge",aes(color=method))+ 
+  stat_summary(size=0.1,aes(color=method),position=position_dodge(width = 0.5)) +
+  theme_bw() +
+  facet_wrap(~set,scale="free") + ylab("rmse") +
+  theme(legend.position = "bottom", legend.title = element_blank(),axis.title.x = element_blank()) +
+  scale_color_brewer(palette = "Dark2")
+ggsave("Four_settings_nodeAge.pdf")
+
+ggplot(d[d$treeTool != "RAxML" & d$measure=="rmse",],aes(x=method,y=value,group=method,fill=method)) +
+  #geom_bar(stat="summary",alpha=0.85) + 
+  #geom_errorbar(stat="summary",width=0.4,aes(color=method)) +
+  stat_summary(size=0.1,aes(color=method)) +
   theme_bw() + facet_wrap(~clockModel+benchmark) + ylab("rmse") +
   theme(axis.text.x = element_blank(), legend.position="bottom",axis.title.x=element_blank(),legend.title=element_blank()) + 
-  scale_fill_brewer(palette = "Accent")
-
+  scale_fill_brewer(palette = "Dark2") +
+  scale_color_brewer(palette = "Dark2")
 ggsave("Combined.pdf")
