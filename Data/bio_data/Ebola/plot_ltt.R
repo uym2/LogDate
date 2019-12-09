556799211960c421b171ece7b046bb2396f29543
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2019-12-08 19:27:08 -0800

    linear-scaling option and Ebola data

---
+++ Data/bio_data/Ebola/plot_ltt.R
@@ -0,0 +1,11 @@
+setwd("~/my_gits/LogDate/Data/bio_data/Ebola/")
+
+require("ggplot2")
+
+d = read.table("ltt_data.txt",header = T)
+
+ggplot(d,aes(x=time,y=lineages,color=method)) + geom_line() + theme_classic() +
+  scale_color_manual(values=c("#1f78b4","#e31a1c","#fb9a99","#fdbf6f")) +
+  theme(legend.position = "None")
+
+ggsave("Ebola_ltt.pdf",height = 3,width = 3)
