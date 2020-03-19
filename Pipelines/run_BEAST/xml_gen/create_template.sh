c2fed69f1bef58cdfb58d1715e99dd394f243277
Mai To Uyen <uym2@Mais-MBP-3.attlocal.net>
2020-03-19 14:53:38 -0700

    Removed Data to save space

--- Pipelines/run_BEAST/xml_gen/create_template.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-python template_gen.py from_lsd/StrictClock/D750_11_10_1.xml templates/strict_clock.xml
-python template_gen.py from_lsd/Lognorm/D750_11_10_1.xml templates/lognormal.xml
--- Pipelines/run_BEAST/xml_gen/examples/D750_11_10_1/create_xml_strict_clock_gamma.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-python ../../xml_gen.py ../../templates/strict_clock.xml sampling_time.txt sequences.gamma.fasta true.gamma.noOG.tre strict_clock.gamma  strict_clock.gamma.xml
